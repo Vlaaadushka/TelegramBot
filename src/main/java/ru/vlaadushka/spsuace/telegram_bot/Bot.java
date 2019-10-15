bf973ce16e5b82c63faa66d0dc87c9f77a4d80dd
vlada <vladastanis@mail.ru>
2019-10-15 23:11:05 +0300

    команда Hello + убрала лишнего бота + добавила InlineKeyboardMarkup + исправила многоразовые ответы на одну коменду или слово

--- src/main/java/ru/vlaadushka/spsuace/telegram_bot/Bot.java
+++ src/main/java/ru/vlaadushka/spsuace/telegram_bot/Bot.java
@@ -6,7 +6,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -23,6 +25,27 @@ public class Bot extends TelegramLongPollingBot {
         super(botOptions);
     }
 
+    public static SendMessage sendInlineKeyBoardMessage(long chatId) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText("Носик");
+        inlineKeyboardButton1.setCallbackData("Котик \"Носик\" любопытный");
+        inlineKeyboardButton2.setText("Хвост");
+        inlineKeyboardButton2.setCallbackData("Котик \"Хвост\" длинный");
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Лапка").setCallbackData("Котик \"Лапка\" пушистая"));
+        keyboardButtonsRow2.add(inlineKeyboardButton2);
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText("Что выберешь ты?").setReplyMarkup(inlineKeyboardMarkup);
+    }
+
+
     @Override
     public void onUpdateReceived(Update update) {
 
@@ -30,24 +53,39 @@ public class Bot extends TelegramLongPollingBot {
         update.getUpdateId();
         SendMessage sendMessage = new SendMessage(message.getChatId(), message.getText()).setChatId(update.getMessage().getChatId());
 
-        sendMessage.setText(String.valueOf(new HelloCommand()));
 
-         if (update.getMessage().getText().equals("Привет")) {
-            sendMessage.setText("Приветствую тебя, человек!");
+
+        if(update.getMessage().getText().equals("Котик")){
             try {
-                execute(sendMessage);
+                execute(sendInlineKeyBoardMessage(update.getMessage().getChatId()));
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         } else {
-            sendMessage.setText("Напиши мне 'Привет', человек, и я может быть отвечу тебе!");
-            try {
-                execute(sendMessage);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            if (update.getMessage().getText().equals("/hello")) {
+                sendMessage.setText(String.valueOf(new HelloCommand()));
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (update.getMessage().getText().equals("Привет")) {
+                sendMessage.setText("Приветствую тебя, человек!");
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                    sendMessage.setText("Напиши мне 'Привет', человек, и я может быть отвечу тебе!");
+                    try {
+                        execute(sendMessage);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
-    }
 
     @Override
     public String getBotUsername() {
